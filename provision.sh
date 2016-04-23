815036c68d4a7f9938f8342829497ea12750e756
masiuchi <masahiro.iuchi@gmail.com>
2016-04-23 20:01:58 +0900

    Change files once.

--- provision.sh
+++ provision.sh
@@ -2,15 +2,15 @@
 
 yum -y install httpd php
 
-ln -s /usr/bin/php-cgi /var/www/cgi-bin/
-
 if [[ ! -f /etc/httpd/conf.d/php-cgi.conf ]]; then
   mv /etc/httpd/conf.d/php.conf /etc/httpd/conf.d/php.conf.d
   cp -rf /vagrant/conf/* /
-fi
 
-rm -rf /var/www/html/
-ln -s /vagrant/html /var/www/
+  ln -s /usr/bin/php-cgi /var/www/cgi-bin/
+
+  rm -rf /var/www/html/
+  ln -s /vagrant/html /var/www/
+fi
 
 service httpd start
 chkconfig --level 345 httpd on
